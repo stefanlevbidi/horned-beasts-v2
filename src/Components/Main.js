8ab53e242af063c0ad94b3746fdb26aa0345cc55
stefanlevbidi <stefanlev21796@gmail.com>
2023-03-03 15:56:53 +0000

    saved

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,17 @@
 import HornedBeast from "./HornedBeast";
 
 export default function Main({ data, handleModal, hornsFilter }) {
+  // .filter is an array method that takes a function as its argument
+  // the function looks for a conditional (in this case beast.horns === parseInt(hornsFilter))
+  // when the condition matches it makes the array only items that match
+  // we put:
+  // !hornsFilter ||
+  // in front of the condition so we ONLY look at that when hornsFilter isn't blank
+  const filteredData = data.filter((beast) => !hornsFilter || beast.horns === parseInt(hornsFilter));
+
   return (
     <main>
-      {data.map((beast) => {
+      {filteredData.map((beast) => {
         return (
           <HornedBeast
             title={beast.title}
