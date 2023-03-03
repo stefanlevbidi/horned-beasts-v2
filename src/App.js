42924c4034fdaf882d70412d1136c940848d8f8b
Tim Smith <tim.smith2814@gmail.com>
2023-03-03 11:58:56 +0000

    start filter functionality

--- src/App.js
+++ src/App.js
@@ -8,20 +8,19 @@ import SelectedBeast from "./Components/SelectedBeast";
 
 function App() {
   const [modalShow, setModalShow] = useState(false);
-  const [theBeast, setTheBeast] = useState({}); // start of stretch goal
-  // stretch goal is to set theBeast to be the entire beast object from the data.json
+  const [theBeast, setTheBeast] = useState({});
+  const [hornsFilter, setHornsFilter] = useState("");
 
   function handleModal(beast) {
     setModalShow(!modalShow);
     setTheBeast(beast);
-    console.log(beast);
   }
 
   // using modalShow and handleModal as props, have it so the SelectedBeast component shows when you click on any of the images
   return (
     <div className="App">
-      <Header />
-      <Main data={data} handleModal={handleModal} />
+      <Header hornsFilter={hornsFilter} setHornsFilter={setHornsFilter} />
+      <Main data={data} handleModal={handleModal} hornsFilter={hornsFilter} />
       {modalShow && <SelectedBeast theBeast={theBeast} handleModal={handleModal} />}
       <Footer />
     </div>
--- src/Components/SelectedBeast.js
+++ src/Components/SelectedBeast.js
@@ -2,12 +2,14 @@ function SelectedBeast({ theBeast, handleModal }) {
   function closeModal() {
     // set theBeast to be an empty object
     handleModal({});
+    console.log("here");
   }
 
   return (
     <div className="selected-beast">
       <span onClick={closeModal}>X</span>
       <img src={theBeast.image_url} alt={theBeast.title} />
+      <p>{theBeast.description}</p>
     </div>
   );
 }
